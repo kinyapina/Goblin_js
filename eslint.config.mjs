c747eb3a6872890b253e537f81086e4a3afa6195
Katya Kinyapina <kinyapina.katya28@gmail.com>
2024-09-13 23:10:39 +0700

    change eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,28 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
+import jest from "eslint-plugin-jest";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+  {
+    ignores: ["dist/*"],
+  },
+  {
+    files: ["**/*.test.js"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+  },
 ];
