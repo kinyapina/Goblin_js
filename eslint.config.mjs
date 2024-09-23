657999053f67cb8426b722bf8d57cf5bdf1fff89
Katya Kinyapina <kinyapina.katya28@gmail.com>
2024-09-23 22:46:13 +0700

    update eslint-config-mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,26 @@
-import config from "eslint-config-standard";
-
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import jest from "eslint-plugin-jest";
 
 export default [
-  ...[].concat(config),
+  {
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+  {
+    ignores: ["dist/*"],
+  },
+  {
+    files: ["**/*.test.js"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+  },
 ];
